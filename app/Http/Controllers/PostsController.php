fb1adfe407fc51832beb0e7c9912b7d64ed7c462
Marina Rozova <marinar578@gmail.com>
2017-05-02 17:12:26 -0400

    Add form to create a post and save the post to the database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Post;
 
 class PostsController extends Controller
 {
@@ -14,5 +15,27 @@ class PostsController extends Controller
   public function show()
   {
     return view('posts.show');
+  }
+
+  public function create()
+  {
+    return view('posts.create');
+  }
+
+  public function store()
+  {
+    // var_dump("hi"); die();
+    // Create a new post using the request data
+    $post = new Post;
+
+    $post->title = request('title');
+    $post->body = request('body');
+
+    // dd(request('title'));
+    // Save it to the database
+    $post->save();
+
+    // And then redirect to the home page
+    return redirect('/');
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts', 'PostsController@store');
 
+// Route::get('/posts/{post}', 'PostsController@show');
+
+// GET /posts
+// GET /posts/create
+// POST /posts
+// GET /posts/{id}/edit 
+// PATCH /posts/{id}
+// GET /posts/{id}
+// DELETE /posts/{id}
