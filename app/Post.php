ee0e79ff1d5e0dd1d58abaf45662a3aa5204c14d
Marina Rozova <marinar578@gmail.com>
2017-05-02 17:16:14 -0400

    Specify another way to create posts with the  variable

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'body'];
+    // protected $fillable = ['title', 'body'];
+    protected $guarded = []; // specify fields you don't want to allow and it filters them out
 }
