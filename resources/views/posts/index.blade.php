2b7ff7f3c4e735cc118e32a37c46c7e554ffcb30
Marina Rozova <marinar578@gmail.com>
2017-05-02 17:46:43 -0400

    Fix minor container issue

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,6 +9,4 @@
       <a class="btn btn-outline-primary" href="#">Older</a>
       <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
     </nav>
-
-  </div><!-- /.blog-main -->
 @endsection
