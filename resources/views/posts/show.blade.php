2aac0b4e086877bf74c6439d01f8c4c1693b095c
Marina Rozova <marinar578@gmail.com>
2017-05-02 12:50:39 -0400

    Create layouts and views with bootstrap templates

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends ('layouts.master')
+
+@section('content')
+  <h1>A place to show your post</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
 Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
 
-
-// controller =>PostsController
-// Eloquent model => Post
-// migration => create_posts_table
