686798ff14d5b2a301fcc1cc630db918a138bbf1
Marina Rozova <marinar578@gmail.com>
2017-05-05 15:10:00 -0400

    Publish email vendor files to have more control over styling emails

--- config/mail.php
+++ config/mail.php
@@ -113,7 +113,7 @@ return [
     */
 
     'markdown' => [
-        'theme' => 'default',
+        'theme' => 'laravel-blog',
 
         'paths' => [
             resource_path('views/vendor/mail'),
