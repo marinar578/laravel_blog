47c40da2d29330bb94ef8500a43c30de0e3d64b7
Marina Rozova <marinar578@gmail.com>
2017-05-08 11:33:34 -0400

    Refactor service provider to have cleaner syntax

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('layouts.sidebar', function ($view) {
-            $view->with('archives', \App\Post::archives());
-            $view->with('tags', \App\Tag::has('posts')->pluck('name'));
+            $archives = \App\Post::archives();
+            $tags = \App\Tag::has('posts')->pluck('name');
+
+            $view->with(compact('archives', 'tags')); 
         });
     }
 
