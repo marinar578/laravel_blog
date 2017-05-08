6991dfc9f07d404a3918fd4390c383d04586b1c8
Marina Rozova <marinar578@gmail.com>
2017-05-08 11:30:05 -0400

    Add tags to the sidebar as long as they have posts associated with them

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('layouts.sidebar', function ($view) {
             $view->with('archives', \App\Post::archives());
+            $view->with('tags', \App\Tag::has('posts')->pluck('name'));
         });
     }
 
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,10 @@
 <div class="col-sm-3 offset-sm-1 blog-sidebar">
+
   <div class="sidebar-module sidebar-module-inset">
     <h4>About</h4>
     <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
   </div>
+
   <div class="sidebar-module">
     <h4>Archives</h4>
     <ol class="list-unstyled">
@@ -15,6 +17,20 @@
       @endforeach
     </ol>
   </div>
+
+  <div class="sidebar-module">
+    <h4>Tags</h4>
+    <ol class="list-unstyled">
+      @foreach ($tags as $tag)
+        <li>
+          <a href="/posts/tags/{{ $tag }}">
+            {{ $tag }}
+          </a>
+        </li>
+      @endforeach
+    </ol>
+  </div>
+
   <div class="sidebar-module">
     <h4>Elsewhere</h4>
     <ol class="list-unstyled">
@@ -23,4 +39,5 @@
       <li><a href="#">Facebook</a></li>
     </ol>
   </div>
+
 </div><!-- /.blog-sidebar -->
